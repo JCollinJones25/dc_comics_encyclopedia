aa2eeca97bb4f41ebd1dbf923b76bc5104973846
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-27 16:56:05 -0400

    batman and flash entered into heroes db using admin and psql

--- main_app/models.py
+++ main_app/models.py
@@ -2,5 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Hero(models.Model):
 
+    name = models.CharField(max_length=100)
+    secret_identity = models.CharField(max_length=100)
+    img = models.CharField(max_length=500)
+    bio = models.TextField(max_length=500)
+    powers = models.CharField(max_length=100)
+    universe = models.CharField(max_length=50)
+    affiliations = models.TextField(max_length=500)
+    villains = models.TextField(max_length=500)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
 
--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from django.http import HttpResponse
-
+from .models import Hero
 
 # Create your views here.
 
@@ -11,3 +11,11 @@ class Home(TemplateView):
 
 class About(TemplateView):
     template_name = 'about.html'
+
+class HeroesList(TemplateView):
+    template_name = 'heroes_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['heroes'] = Hero.objects.all()
+        return context
