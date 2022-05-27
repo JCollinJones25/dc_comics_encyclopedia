e963dbf16f1e8f8189e01e1761164a8e5b2cd6bc
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-27 15:21:26 -0400

    more files

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views import View
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+class Home(View):
+    template_name = 'home.html'
+
+class About(View):
+    template_name = 'about_html'
